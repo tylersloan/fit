fae97feba487060c4f3c0d41c1ccb8ba9915d11e
Tyler Sloan <tsloan@insiderealestate.com>
2025-01-21 13:47:39 -0500

    update settings

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
